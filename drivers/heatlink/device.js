01524203167dc1fe955cb51d469e5e1f30c58ea1
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-07-17 13:38:06 +0200

    update console.logs

--- drivers/heatlink/device.js
+++ drivers/heatlink/device.js
@@ -5,13 +5,20 @@ module.exports = class Heatlink extends Homey.Device {
     onInit() {
         this.registerCapabilityListener('target_temperature', this.onCapabilityTargetTemperature.bind(this));
         homewizardConnection.registerListener(() => {
-            console.log('data available', this.getCapabilityValue('measure_temperature'), homewizardConnection.heatlink.roomTemperature);
-            this.setCapabilityValue('target_temperature', homewizardConnection.heatlink.heatingTemperature).catch( (e) => console.log('a',e) );
-            this.setCapabilityValue('measure_temperature', homewizardConnection.heatlink.roomTemperature).catch( (e) => console.log('e',e) );
+            console.log('update avaiable')
+            if (this.getCapabilityValue('measure_temperature') !== homewizardConnection.heatlink.roomTemperature) {
+                console.log('updated measure_temperature to', homewizardConnection.heatlink.roomTemperature);
+;                this.setCapabilityValue('measure_temperature', homewizardConnection.heatlink.roomTemperature)
+            }
+
+            if (this.getCapabilityValue('target_temperature') !== homewizardConnection.heatlink.heatingTemperature) {
+                console.log('updated target_temperature to', homewizardConnection.heatlink.heatingTemperature);
+                this.setCapabilityValue('target_temperature', homewizardConnection.heatlink.heatingTemperature)
+            }
         });
     }
 
-    async onCapabilityTargetTemperature(value, opts) {
+    async onCapabilityTargetTemperature(value) {
         await homewizardConnection.setTemperature(value);
         this.setCapabilityValue('target_temperature', homewizardConnection.heatlink.heatingTemperature);
     }
