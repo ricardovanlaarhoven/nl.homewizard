ca563b392ab02a8e45dbf58faaf990e996f67735
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-07-31 16:37:44 +0200

    remove console.logs

--- drivers/heatlink/device.js
+++ drivers/heatlink/device.js
@@ -7,16 +7,16 @@ module.exports = class Heatlink extends Homey.Device {
         this.registerCapabilityListener('target_temperature', this.onCapabilityTargetTemperature.bind(this));
         homewizardConnection.registerListener(() => {
             if (this.getCapabilityValue('measure_temperature') !== homewizardConnection.heatlink.roomTemperature) {
-                this.setCapabilityValue('measure_temperature', homewizardConnection.heatlink.roomTemperature).catch((e) => {console.log('error1', e)});
+                this.setCapabilityValue('measure_temperature', homewizardConnection.heatlink.roomTemperature);
             }
             if (this.getCapabilityValue('target_temperature') !== homewizardConnection.heatlink.heatingTemperature) {
-                this.setCapabilityValue('target_temperature', homewizardConnection.heatlink.heatingTemperature).catch((e) => {console.log('error2', e)});
+                this.setCapabilityValue('target_temperature', homewizardConnection.heatlink.heatingTemperature);
             }
             if (this.getCapabilityValue('measure_water_temperature') !== homewizardConnection.heatlink.waterTemperature) {
-                this.setCapabilityValue('measure_water_temperature', homewizardConnection.heatlink.waterTemperature).catch((e) => {console.log('error3', e)});
+                this.setCapabilityValue('measure_water_temperature', homewizardConnection.heatlink.waterTemperature);
             }
             if (this.getCapabilityValue('measure_water_pressure') !== homewizardConnection.heatlink.waterPressure) {
-                this.setCapabilityValue('measure_water_pressure', homewizardConnection.heatlink.waterPressure).catch((e) => {console.log('error4', e)});
+                this.setCapabilityValue('measure_water_pressure', homewizardConnection.heatlink.waterPressure);
             }
         });
     }
