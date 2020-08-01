6afe20eff39dba18930e4cf8e9e4a351246befdd
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-08-01 18:34:27 +0200

    remove comments

--- app/models/Energylink.js
+++ app/models/Energylink.js
@@ -14,36 +14,3 @@ module.exports = function HeatlinkModel(energylink) {
         }
     }
 };
-
-/*
-[
-    {
-        "id": 0,
-        "tariff": 1,
-        "s1": null,
-        "s2": null,
-        "aggregate": {
-            "po": 1382,
-            "dayTotal": 2.22,
-            "po+": 1454,
-            "po+t": "10:05",
-            "po-": 90,
-            "po-t": "06:01"
-        },
-        "used": {
-            "po": 1382,
-            "dayTotal": 2.23,
-            "po+": 1454,
-            "po+t": "10:05",
-            "po-": 90,
-            "po-t": "06:01"
-        },
-        "gas": {
-            "lastHour": 0.00,
-            "dayTotal": 0.34
-        },
-        "kwhindex": 0.00
-    }
-]
-
- */
